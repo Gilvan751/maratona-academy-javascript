bc5a4144e283c262d16404e9fc111b630d84a35d
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-17 16:25:30 -0300

    Instruções gerais sobre as funções do javascript

--- variaveis/script.js
+++ variaveis/script.js
@@ -1,1 +1,60 @@
-alert('ola mundo!')
+// tipos de declaração de variaveis e verificação
+
+/* let nome = 'Gilvan';
+let idade = 50;
+let texto = 'Essa é uma pessoa boa!';
+let description;
+
+document.write(nome);
+document.write(idade);
+document.write(texto);
+console.log(nome);
+console.log(idade);
+console.log(texto);
+console.log(description); */
+
+// tipos de dados
+/* String, boleano, numerica, função typeof para verificar que tip de dado é  */
+/* console.log(typeof (nome)); */
+/* let nomes = prompt('Digite um nome');
+console.log('Bom dia ' + nomes + ', hoje esta tudo perfeito'); */
+
+// contador =+1 ou contador ++ ou contador = contador + 1
+
+//ARRAYS
+/* let sprints = ['Ambiete dev', 'HTML', 'javascript', 'CSS', 'Git e Git Hub']
+console.log(sprints.length)
+
+let todo = [['levantar', 'escovar', 'tomar café'], ['almoçar', 'ir trabalhar'], ['retornar do serviço', 'estudar jacascript']]
+console.log(todo[0][2])
+console.log(todo.length)
+todo[2].push('tomar um banho para ir dormir','ver um menu') // push para adicionar elemento dentro da lista
+console.log(todo[2])
+todo[2].unshift('esse e o primeiro elemento da lista')// unshift adiciona o elemento no incio da lista
+console.log(todo[2])
+let manha = todo[0];
+let tarde = todo[1];
+let noite = todo[2];
+console.log(manha)
+console.log(tarde)
+console.log(noite)
+noite.pop()   // o pop remove o último elemento da lista
+console.log(noite)
+noite.shift() // o shift remove o primeiro elemento da lista
+console.log(noite) */
+// é para trabalhar com listas
+/* let aluno = {
+    nome: 'Gilvan',
+    sobrenome: 'oliveira Silva',
+    status: true,
+    modalidade: ['musculação', 'natação','karate']
+}
+console.log(aluno.modalidade[2])
+aluno.nome = "pedro"
+aluno.idade = 50
+delete aluno.sobrenome
+console.log(aluno) */
+
+// Trabalhar com Arrays e Objetos
+let users = [{ id: 1, nome: 'Gilvan', idade: 50 }, { id: 2, nome: 'Pedro', idade: 40 }, { id: 3, nome: 'Marta', idade: 18 }]
+console.log(users)
