b927cac73352bb2e9d89c795bb6b020f3c64dee1
Gilvan OLiveira Silva <gilvanguru@gmail.com>
2022-05-18 11:49:36 -0300

    Variaveis java script para funções

---
+++ javascript-no-html/script.js
@@ -0,0 +1,16 @@
+// funções globais são as que já vem no javascript
+//Number = numero , parseFloat =numebr float, String = texto
+
+/* let variaveis = {
+    tamanho: 10,     ///propriedade e metodos que são as funções abaixo
+    somaVariavel: () => {
+        return 10 + 10
+    }
+}
+//propriedade.metodo
+console.log(variaveis.somaVariavel()) */
+/* function soma1() {
+    console.log('Estou somando mais 1')
+}
+soma1() */
+document.getElementById('button').innerText = "clicou"
